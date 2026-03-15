40a0fe601d132f5b458a85c024e15caa06886e73
Muratbekova Aruuke <muratbekovaaruuke01@kstu.kg>
2026-03-16 02:01:57 +0600

    еще один коммит после merge

--- main.go
+++ main.go
@@ -3,12 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	//HEAD
-	fmt.Println("Hello Git")
-	fmt.Println("Hello Aruuke")
-
-	text := "I love dogs"
-	fmt.Println(text)
-	//feature
-
+	fmt.Println("IVT 1-25")
 }
